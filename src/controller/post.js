53fba688c8ec6a7fb80361d4345b3828ab746b55
Mychaelson <sonmychael@gmail.com>
2022-04-13 17:13:32 +0700

    ref: added service layer for post endpoint

--- src/controller/post.js
+++ src/controller/post.js
@@ -2,202 +2,103 @@ const { Post, User, Comment, Like } = require("../lib/sequelize");
 const serverErrorHandler = require("../lib/serverErrorHandler");
 const fs = require("fs");
 const uploadFileDomain = require("../config/uploadFile");
+const PostService = require("../services/post");
 
 const postControllers = {
   getAllPost: async (req, res) => {
     try {
-      const { _limit = 10, _page = 1, _sortBy = "", _sortDir = "" } = req.query;
+      const serviceResult = await PostService.getAllPost(req);
 
-      delete req.query._limit;
-      delete req.query._page;
-      delete req.query._sortBy;
-      delete req.query._sortDir;
+      if (!serviceResult.success) throw serviceResult;
 
-      const result = await Post.findAndCountAll({
-        where: {
-          ...req.query,
-        },
-        limit: _limit ? parseInt(_limit) : undefined,
-        offset: (_page - 1) * _limit,
-        include: [
-          {
-            model: User,
-            as: "user_posts",
-          },
-          {
-            model: Comment,
-            include: User,
-            limit: 5,
-            order: [["createdAt", "DESC"]],
-          },
-          {
-            model: User,
-            as: "post_like",
-            where: {
-              id: req.token.id,
-            },
-            required: false,
-          },
-        ],
-        distinct: true,
-        order: _sortBy ? [[_sortBy, _sortDir]] : undefined,
-      });
-
-      if (!result.rows.length) {
-        return res.status(400).json({
-          message: "There are no post yet",
-        });
-      }
-
-      return res.status(200).json({
-        message: "find all post",
-        result,
+      return res.status(serviceResult.statusCode || 200).json({
+        message: serviceResult.message,
+        result: serviceResult.data,
       });
     } catch (err) {
-      serverErrorHandler(err, req, res);
+      return res.status(err.statusCode || 500).json({
+        message: err.message,
+      });
     }
   },
   createNewPost: async (req, res) => {
     try {
-      const { caption, location, user_id } = req.body;
+      const serviceResult = await PostService.createNewPost(req);
 
-      const uploadeFileDomain = uploadFileDomain;
-      const filePath = `post_images`;
-      const { filename } = req.file;
+      if (!serviceResult.success) throw serviceResult;
 
-      const newPost = await Post.create({
-        image_url: `${uploadeFileDomain}/${filePath}/${filename}`,
-        caption,
-        location,
-        user_id: req.token.id,
-      });
-
-      return res.status(200).json({
-        message: "Post Successful",
+      return res.status(serviceResult.statusCode || 200).json({
+        message: serviceResult.message,
+        result: serviceResult.data,
       });
     } catch (err) {
-      fs.unlinkSync(__dirname + "/../public/posts/" + req.file.filename);
-      serverErrorHandler(err, req, res);
+      return res.status(err.statusCode || 500).json({
+        message: err.message,
+      });
     }
   },
   editPost: async (req, res) => {
     try {
-      const { id } = req.params;
+      const serviceResult = await PostService.editPost(req);
 
-      const editPost = await Post.update(
-        {
-          ...req.body,
-        },
-        {
-          where: {
-            id,
-            user_id: req.token.id,
-          },
-        }
-      );
+      if (!serviceResult.success) throw serviceResult;
 
-      return res.status(200).json({
-        message: "Post Edited",
+      return res.status(serviceResult.statusCode || 200).json({
+        message: serviceResult.message,
+        result: serviceResult.data,
       });
     } catch (err) {
-      serverErrorHandler(err, req, res);
+      return res.status(err.statusCode || 500).json({
+        message: err.message,
+      });
     }
   },
   deletePost: async (req, res) => {
     try {
-      const { id } = req.params;
+      const serviceResult = await PostService.deletePost(req);
 
-      const findPost = await Post.findOne({
-        where: {
-          id,
-        },
-      });
+      if (!serviceResult.success) throw serviceResult;
 
-      const deletePost = await Post.destroy({
-        where: {
-          id,
-          user_id: req.token?.id,
-        },
-      });
-
-      const postImgUrl = findPost.dataValues.image_url;
-      const postImgSplitName = postImgUrl.split("/");
-
-      fs.unlinkSync(
-        __dirname +
-          "/../public/posts/" +
-          postImgSplitName[postImgSplitName.length - 1]
-      );
-
-      res.status(200).json({
-        message: "Post Deleted",
+      return res.status(serviceResult.statusCode || 200).json({
+        message: serviceResult.message,
+        result: serviceResult.data,
       });
     } catch (err) {
-      serverErrorHandler(err, req, res);
+      return res.status(err.statusCode || 500).json({
+        message: err.message,
+      });
     }
   },
   incrementPostLikes: async (req, res) => {
     try {
-      const { post_id, user_id } = req.params;
+      const serviceResult = await PostService.incrementPostLikes(req);
 
-      const hasUserLikeThePost = await Like.findOne({
-        where: {
-          user_id,
-          post_id,
-        },
-      });
+      if (!serviceResult.success) throw serviceResult;
 
-      if (hasUserLikeThePost) {
-        return res.status(400).json({
-          message: "User has liked the post",
-        });
-      }
-
-      const likePost = await Like.create({
-        post_id,
-        user_id: req.token.id,
-      });
-
-      await Post.increment({ like_count: 1 }, { where: { id: post_id } });
-
-      return res.status(200).json({
-        message: "Like added",
+      return res.status(serviceResult.statusCode || 200).json({
+        message: serviceResult.message,
+        result: serviceResult.data,
       });
     } catch (err) {
-      serverErrorHandler(err, req, res);
+      return res.status(err.statusCode || 500).json({
+        message: err.message,
+      });
     }
   },
   decrementPostLikes: async (req, res) => {
     try {
-      const { user_id, post_id } = req.params;
+      const serviceResult = await PostService.decrementPostLikes(req);
 
-      const hasUserLikeThePost = await Like.findOne({
-        where: {
-          user_id,
-          post_id,
-        },
-      });
+      if (!serviceResult.success) throw serviceResult;
 
-      if (!hasUserLikeThePost) {
-        return res.status(400).json({
-          message: "User have not like the post",
-        });
-      }
-
-      const deleteLike = await Like.destroy({
-        where: {
-          user_id: req.token.id,
-          post_id,
-        },
-      });
-
-      await Post.increment({ like_count: -1 }, { where: { id: post_id } });
-
-      return res.status(200).json({
-        message: "Like Deleted",
+      return res.status(serviceResult.statusCode || 200).json({
+        message: serviceResult.message,
+        result: serviceResult.data,
       });
     } catch (err) {
-      serverErrorHandler(err, req, res);
+      return res.status(err.statusCode || 500).json({
+        message: err.message,
+      });
     }
   },
   // getAllUserthatLikeThePost: async (req, res) => {},
---
+++ src/services/post/index.js
@@ -0,0 +1,242 @@
+const uploadFileDomain = require("../../config/uploadFile");
+const { Post, User, Comment, Like } = require("../../lib/sequelize");
+const Service = require("../service");
+const fs = require("fs");
+
+class PostService extends Service {
+  static getAllPost = async (req) => {
+    try {
+      const { _limit = 10, _page = 1, _sortBy = "", _sortDir = "" } = req.query;
+
+      delete req.query._limit;
+      delete req.query._page;
+      delete req.query._sortBy;
+      delete req.query._sortDir;
+
+      const result = await Post.findAndCountAll({
+        where: {
+          ...req.query,
+        },
+        limit: _limit ? parseInt(_limit) : undefined,
+        offset: (_page - 1) * _limit,
+        include: [
+          {
+            model: User,
+            as: "user_posts",
+          },
+          {
+            model: Comment,
+            include: User,
+            limit: 5,
+            order: [["createdAt", "DESC"]],
+          },
+          {
+            model: User,
+            as: "post_like",
+            where: {
+              id: req.token.id,
+            },
+            required: false,
+          },
+        ],
+        distinct: true,
+        order: _sortBy ? [[_sortBy, _sortDir]] : undefined,
+      });
+
+      if (!result.rows.length) {
+        return this.handleError({
+          statusCode: 400,
+          message: "There are no post yet!",
+        });
+      }
+
+      return this.handleSuccess({
+        statusCode: 200,
+        message: "Find all post!",
+        data: result,
+      });
+    } catch (err) {
+      console.log(err);
+      return this.handleError({
+        message: "Server Error",
+        statusCode: 500,
+      });
+    }
+  };
+
+  static createNewPost = async (req) => {
+    try {
+      const { caption, location, user_id } = req.body;
+
+      const uploadeFileDomain = uploadFileDomain;
+      const filePath = `post_images`;
+      const { filename } = req.file;
+
+      const newPost = await Post.create({
+        image_url: `${uploadeFileDomain}/${filePath}/${filename}`,
+        caption,
+        location,
+        user_id: req.token.id,
+      });
+
+      return this.handleSuccess({
+        message: "Create Post Successful!",
+        statusCode: 201,
+      });
+    } catch (err) {
+      fs.unlinkSync(__dirname + "/../../public/posts/" + req.file.filename);
+      console.log(err);
+      return this.handleError({
+        message: "Server Error",
+        statusCode: 500,
+      });
+    }
+  };
+
+  static editPost = async (req) => {
+    try {
+      const { id } = req.params;
+
+      const editPost = await Post.update(
+        {
+          ...req.body,
+        },
+        {
+          where: {
+            id,
+            user_id: req.token.id,
+          },
+        }
+      );
+
+      return this.handleSuccess({
+        message: "Post Edited!",
+        statusCode: 200,
+      });
+    } catch (err) {
+      console.log(err);
+      return this.handleError({
+        message: "Server Error",
+        statusCode: 500,
+      });
+    }
+  };
+
+  static deletePost = async (req) => {
+    try {
+      const { id } = req.params;
+
+      const findPost = await Post.findOne({
+        where: {
+          id,
+        },
+      });
+
+      const deletePost = await Post.destroy({
+        where: {
+          id,
+          user_id: req.token?.id,
+        },
+      });
+
+      const postImgUrl = findPost.dataValues.image_url;
+      const postImgSplitName = postImgUrl.split("/");
+
+      fs.unlinkSync(
+        __dirname +
+          "/../../public/posts/" +
+          postImgSplitName[postImgSplitName.length - 1]
+      );
+
+      return this.handleSuccess({
+        message: "Post Deleted!",
+      });
+    } catch (err) {
+      console.log(err);
+      return this.handleError({
+        message: "Server Error",
+        statusCode: 500,
+      });
+    }
+  };
+
+  static incrementPostLikes = async (req) => {
+    try {
+      const { post_id, user_id } = req.params;
+
+      const hasUserLikeThePost = await Like.findOne({
+        where: {
+          user_id,
+          post_id,
+        },
+      });
+
+      if (hasUserLikeThePost) {
+        return this.handleError({
+          message: "User has liked the post",
+          statusCode: 400,
+        });
+      }
+
+      const likePost = await Like.create({
+        post_id,
+        user_id: req.token.id,
+      });
+
+      await Post.increment({ like_count: 1 }, { where: { id: post_id } });
+
+      return this.handleSuccess({
+        message: "Like Added!",
+        statusCode: 200,
+      });
+    } catch (err) {
+      console.log(err);
+      return this.handleError({
+        message: "Server Error",
+        statusCode: 500,
+      });
+    }
+  };
+
+  static decrementPostLikes = async (req) => {
+    try {
+      const { user_id, post_id } = req.params;
+
+      const hasUserLikeThePost = await Like.findOne({
+        where: {
+          user_id,
+          post_id,
+        },
+      });
+
+      if (!hasUserLikeThePost) {
+        return this.handleError({
+          message: "User have not like the post!",
+          statusCode: 400,
+        });
+      }
+
+      const deleteLike = await Like.destroy({
+        where: {
+          user_id: req.token.id,
+          post_id,
+        },
+      });
+
+      await Post.increment({ like_count: -1 }, { where: { id: post_id } });
+
+      return this.handleSuccess({
+        message: "Like Deleted!",
+        statusCode: 200,
+      });
+    } catch (err) {
+      console.log(err);
+      return this.handleError({
+        message: "Server Error",
+        statusCode: 500,
+      });
+    }
+  };
+}
+
+module.exports = PostService;
