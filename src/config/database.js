d71a76358bba466b3a39f4d8b8563b93cba83177
Mychael Son <98306105+Mychaelson@users.noreply.github.com>
2022-04-23 13:37:14 +0700

    docs: added documentation for config

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,4 @@
+// to orgianzie the config for database, it is gathered here then exported as one object
 const mysqlConfig = {
   MYSQL_USERNAME: process.env.MYSQL_USERNAME,
   MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
